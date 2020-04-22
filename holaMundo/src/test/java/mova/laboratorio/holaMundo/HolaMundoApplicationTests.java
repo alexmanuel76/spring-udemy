f216802ff7824a321b5d1329ac7e39b10df7c800
Alexander Mogollon <alexandermogollon3076@yahoo.com>
2020-04-22 01:11:00 -0400

    Pruebas 1 de Udemy & GIT

---
+++ holaMundo/src/test/java/mova/laboratorio/holaMundo/HolaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package mova.laboratorio.holaMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
